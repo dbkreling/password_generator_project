98ce092df6e56d194a597f23e1fe99f4f1631213
Daniel Kreling <dbkreling@gmail.com>
2020-08-26 21:59:08 +0200

    lecture 28: use form data
    This commit inserts code to make the password consume the options provided
    by the user in the home.html page.
    
    The changes are all in views.py, inside the password function and makes use
    of the data that is contained inside the 'request' parameter, coming to
    password() from the form in home.html.
    
    For further information on the request object (HttpRequest object), please
    refer to https://docs.djangoproject.com/en/3.1/ref/request-response/
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- generator/views.py
+++ generator/views.py
@@ -5,10 +5,16 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-
-    length=12
+    length=int(request.GET.get('length', 12))
     possible_options = list('abcdefghijklmnopqrstuvwxyz')
     the_passoword=''
+
+    if request.GET.get('uppercase'):
+        possible_options.extend('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    if request.GET.get('numbers'):
+        possible_options.extend('1234567890')
+    if request.GET.get('special'):
+        possible_options.extend('!@#$%&^~?*()')
 
     for x in range(length):
         the_passoword += random.choice(possible_options)
