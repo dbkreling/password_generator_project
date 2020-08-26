6c525dc786d004fc526cf0746f3517d1fb636216
Daniel Kreling <dbkreling@gmail.com>
2020-08-26 20:25:13 +0200

    lecture 27: create a form to call the password page
    Also create the password.html file.
    
    The urls.py file contains a map to the data in our site. The structure of
    each path works like this:
    
    path('string_in_browser_url', views.function_name_in_views_py, name='name_used_in_home_html'),
    
    So our files should contain:
    
    Views.py: a function called "function_name_in_views_py" that calls the .html file
    in our app template directory (password.html in our case).
    
    Home.html: contains some html that will collect the data and pass it to the
    url name specified in the 'action' attribute of the form html element. This
    name inside {% url 'url_name' %} must match the name attribute used in views.py.
    
    The name of the element 'select' is a string that will be placed in the url bar
    in your browser as soon as the Generate Password button (input type submit) is
    pressed.
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- generator/views.py
+++ generator/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'generator/home.html')
+
+def password(request):
+    return render(request, 'generator/password.html')
