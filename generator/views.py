4fd1a5c74a3af755adb53eeb6e97e16d17340a78
Daniel Kreling <dbkreling@gmail.com>
2020-08-26 21:06:09 +0200

    lecture 28: create a random password
    This commit introduces the changes to create a random password every time we
    reload the password.html page.
    
    The changes are as so:
    
    home.html:
      - add more options for the password length and make 12 as the 'default' value;
      - add checkboxes for users' choices.
    
    views.py:
      - add the logic to create a random password;
      - add the dictionary with the key:value pair we want to pass to password.html
    
    password.html:
      - add the key name coming from views.py ('password') in order to print it.
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+import random
 
 def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    length=12
+    possible_options = list('abcdefghijklmnopqrstuvwxyz')
+    the_passoword=''
+
+    for x in range(length):
+        the_passoword += random.choice(possible_options)
+
+    return render(request, 'generator/password.html', {'password':the_passoword})
